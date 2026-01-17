fd4652c550c12cd3f755e6e4e3bb76ea59f89d89
Piyush Saini <findpiyushsaini@gmail.com>
2026-01-17 10:19:17 +0530

    Connect to MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+//console.log(process.env.MONGO_URI);
 
 const app = express(); //expressFunc().("/route",(request,response) => {})
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 //Endpoint: it is a combination of a URL + HTTP method that lets the client interact with a specific resource
 //routes
-
 app.use("/api/notes", notesRoutes); //if u send a request to /api/routes, its gonna hit the router in notesRouter.js
 
-app.listen(5001, () => {
-  console.log("Server started on PORT:5001");
+app.listen(PORT, () => {
+  console.log("Server started on PORT:", PORT);
 });
