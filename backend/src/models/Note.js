8b086b019bce4c6eff355d52f53c33f4b8c4f73b
Piyush Saini <findpiyushsaini@gmail.com>
2026-01-17 10:30:11 +0530

    Create Model

---
+++ backend/src/models/Note.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+//1 create schema
+//2 model based off of that schema
+
+// Schema takes 2 parameters:
+// First {} = field definitions (data structure: title, content, etc.)
+// Second {} = schema options (configuration: timestamps, collection name, etc.)
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } //createdAt, updatedAt
+);
+
+//Model based on above schema
+// create a "Note model" based on this "schema"
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
