da99e0b9273995c4bf410166a2df9703852abf1a
Piyush Saini <findpiyushsaini@gmail.com>
2026-01-20 18:29:05 +0530

    Add Tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,13 @@ import toast from "react-hot-toast";
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Congrats")}> Click Me</button>
+      <button
+        onClick={() => toast.success("Congrats")}
+        className="text-red-500 p-4 bg-pink-300"
+      >
+        {" "}
+        Click Me
+      </button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
