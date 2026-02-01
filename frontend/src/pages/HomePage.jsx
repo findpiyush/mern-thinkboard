135cffaae3c73bea861891fca0a3b5fecc0d5d40
Piyush Saini <findpiyushsaini@gmail.com>
2026-02-01 18:43:17 +0530

    [feat] finish HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import axios from "axios";
 import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -39,8 +40,15 @@ const HomePage = () => {
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {true && (
+        {loading && (
           <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
         )}
       </div>
       HomePage
