b3fe41574353d35558cf9a2dee6478b4b82ede63
Piyush Saini <findpiyushsaini@gmail.com>
2026-02-01 17:43:55 +0530

    [frontend] fetch api

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/api/notes");
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching notes");
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
